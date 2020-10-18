65536a16afc4d0b148e4e45e5d1df09df2108f08
Ivan <example@gmail.com>
2020-10-18 19:43:30 +0300

    less pyat

--- js/script.js
+++ js/script.js
@@ -1,4 +1,19 @@
-//"use sctrict";
+const numberOfFilms = +prompt('Сколько фильмов вы посмотрели?', '');
 
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: {},
+    privat: false
+};
 
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+b = prompt('На сколько оцените его?', ''),
+c = prompt('Один из последних просмотренных фильмов?', ''),
+d = prompt('На сколько оцените его?', '');
 
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
